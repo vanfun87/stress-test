7d1f40f25bc691b6054af3c13cb9cfdec7249a01
Ginkgoch <ginkgoch@outlook.com>
2021-06-27 18:10:37 +0800

    refactor

--- pkg/templates/http.go
+++ pkg/templates/http.go
@@ -6,25 +6,8 @@ import (
 	"net/http"
 )
 
-var client *http.Client
-
-func init() {
-	// tr := &http.Transport{
-	// 	DialContext: (&net.Dialer{
-	// 		Timeout:   30 * time.Second,
-	// 		KeepAlive: 30 * time.Second,
-	// 	}).DialContext,
-	// 	MaxIdleConns:        0,                // 最大连接数,默认0无穷大
-	// 	MaxIdleConnsPerHost: 1,                // 对每个host的最大连接数量(MaxIdleConnsPerHost<=MaxIdleConns)
-	// 	IdleConnTimeout:     90 * time.Second, // 多长时间未使用自动关闭连接
-	// 	TLSClientConfig:     &tls.Config{InsecureSkipVerify: true},
-	// }
-	client = &http.Client{
-		// Transport: tr,
-	}
-}
-
 func HttpGet(request *http.Request) error {
+	client := &http.Client{}
 	res, err := client.Do(request)
 	if err != nil {
 		return err
